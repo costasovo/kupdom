9b71341150445d03a4d1089f639947edf6f68058
Rostislav Wolny <rostislav.wolny@sygictravel.com>
2016-12-04 21:31:38 +0100

    Checkboxes are back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: ['h1 span { color: #026ecc; }']
+  styles: ['h1 span { color: #026ecc; }', 'h1 { margin-top: 1rem; }']
 })
 export class AppComponent {
   isDev: boolean = !environment.production;
